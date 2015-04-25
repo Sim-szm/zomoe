194199f26fbfd680c894e70057b79d68af6734e2
zomoe <xianszm007@gmail.com>
2015-04-25 17:35:29 +0800

    main directory

---
+++ zomoe/log_module/hello.cc
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.cc
+ *        Version:  1.0
+ *        Created:  2015年04月25日 17时26分47秒
+ *       Revision:  none
+ *       Compiler:  clang
+ *         Author:  sim szm, xianszm007@gmail.com
+ *        Company:  Class 1107 of Computer Science and Technology
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+int main(int argc, char *argv[])
+{
+	std::cout << "hello zomoe ! " <<std::endl;
+	return 0;
+}
+
